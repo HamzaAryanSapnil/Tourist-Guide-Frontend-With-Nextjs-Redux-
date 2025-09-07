88874b4189b7cd51e16ae39145114b1c9933b5af
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2025-09-07 12:29:40 +0600

    all tours done for now

--- src/components/modules/tours/ToursCards.tsx
+++ src/components/modules/tours/ToursCards.tsx
@@ -7,9 +7,9 @@ export default function TourCard() {
   const tours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
-    <div className="space-y-4">
+    <div className="grid grid-cols-1 md:grid-cols-2  gap-4">
       {tours.map((tour) => (
-        <Card key={tour} className="p-6 shadow-sm">
+        <Card key={tour} className="p-6 shadow-sm min-h-96">
           <h2 className="font-semibold text-lg">Tour Package {tour}</h2>
           <p className="text-muted-foreground">
             Short description of the tour goes here...
